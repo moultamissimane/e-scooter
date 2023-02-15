b92dc5c55895e0d1bbbed11b9f298122889357cb
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-15 14:55:22 +0100

    add src folder

---
+++ src/store/User/index.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState: any = {
+    isAuth: false,
+}
+
+export const UserSlice: any = createSlice({
+    name: 'User',
+    initialState,
+    reducers: {
+        isLogin: (state: any, action: any) => {
+            state.isAuth = action.payload
+        },
+
+    },
+});
+
+export const { isLogin }: any = UserSlice.actions;
+
+export const User = UserSlice.reducer 
---
+++ src/store/index.ts
@@ -0,0 +1,2 @@
+export { User } from './User';
+export { Command } from './Command';
