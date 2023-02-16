52b4d8d39c896e6e75c83cbcce0fa9664b56b830
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-16 21:26:23 +0100

    index component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,9 +1,9 @@
-export { MapButton } from "./navigation/MapButton";
-export { TabBarIcon } from "./navigation/TabBarIcon";
-export { TextApp as Text } from "./other/Text";
-export { SafeScreen } from "./safeArea";
-export { Hike } from "./cards/Hike";
-export { AddCard } from "./cards/AddCard";
-export { ExploreHike } from "./cards/ExploreHike";
-export { RealTripCard } from "./cards/RealTripCard";
-export { Layout } from "./Layout";
+// export { MapButton } from "./navigation/MapButton";
+// export { TabBarIcon } from "./navigation/TabBarIcon";
+// export { TextApp as Text } from "./other/Text";
+// export { SafeScreen } from "./safeArea";
+// export { Hike } from "./cards/Hike";
+// export { AddCard } from "./cards/AddCard";
+// export { ExploreHike } from "./cards/ExploreHike";
+// export { RealTripCard } from "./cards/RealTripCard";
+// export { Layout } from "./Layout";
