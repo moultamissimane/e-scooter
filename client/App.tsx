5f614565673c4700cc0dd1ed14f42df3930e8b7a
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-16 21:31:09 +0100

    update app in client

--- client/App.tsx
+++ client/App.tsx
@@ -1,35 +1,48 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import Navigation from "./src/navigation";
-import { ToastProvider } from 'react-native-toast-notifications'
-import { Provider as ProviderRedux } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import { Command, User } from "./src/store";
+import React from "react";
+import { AppRegistry } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "@/pages/Home";
+import Welcome from "@/pages/Welcome";
+import Login from "@/pages/Login";
+import Signup from "@/pages/Signup";
 
-const store: any = configureStore({
-  reducer: {
-    user: User,
-    command: Command
-  },
-})
-
-
+const Stack = createNativeStackNavigator();
 const App = () => {
-
   return (
-    <ProviderRedux store={store}>
-
-      <ToastProvider
-        successColor="#526FFF"
-        textStyle={{ fontSize: 14 }}
-
-      >
-        <Navigation />
-      </ToastProvider>
-    </ProviderRedux>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-
+};
 
 export default App;
