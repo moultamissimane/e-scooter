1c753a70425a4ec896654530062e6ce15a130579
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-20 20:15:12 +0100

    edit home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import MapboxGL from "@react-native-mapbox-gl/maps";
 
 export default function Home() {
+  MapboxGL.setAccessToken("YOUR_MAPBOX_ACCESS_TOKEN");
+  const [coordinates] = useState([78.9629, 20.5937]);
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.page}>
+    <View style={styles.container}>
+      <MapboxGL.MapView style={styles.map}>
+        <MapboxGL.Camera
+          zoomLevel={4}
+          centerCoordinate={coordinates}
+        />
+        <MapboxGL.PointAnnotation coordinate={coordinates} />
+      </MapboxGL.MapView>
     </View>
-  )
+  </View>
+);
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  container: {
+  height: '100%',
+    width: '100%',
+    backgroundColor: 'blue',
+  },
+  map: {
+    flex: 1
+  }
+})
