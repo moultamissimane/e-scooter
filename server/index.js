601fdc485a11ecc44cd790e3d15191fbad5d7af3
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-23 09:36:23 +0100

    edit index in server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
-// import express from "express";
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const startServer = require("./Config/Db").startServer; 
+const startServer = require("./Config/Db").startServer;
 const json = require("express").json;
-const User  = require("./Models/UserModel");
+const User = require("./Models/UserModel");
 const AuthRoutes = require("./Routes/UserRoutes");
 const requireToken = require("./middleware/AuthMiddleware");
+const ScooterRoutes = require("./Routes/ScooterRoutes");
 
 app.use(json());
 
 app.use(AuthRoutes);
+app.use(ScooterRoutes);
 
 app.get("/", requireToken, (req, res) => {
   console.log(req.user);
@@ -22,4 +23,3 @@ app.listen(PORT, () => {
 });
 
 startServer();
-
