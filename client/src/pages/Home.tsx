057c35dd532ac22dfb0e059c66ccd891304fbd86
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-23 09:42:34 +0100

    edit home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
-
+import Map from "../components/Map";
 export default function Home() {
  
   return (
     <View style={styles.page}>
-      
+      <Map />
     </View>
   );
 }
