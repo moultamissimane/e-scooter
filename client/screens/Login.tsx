5fc52589207f377edc08b5d21bc61402b50267de
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-28 10:15:43 +0100

    edit login page

--- client/screens/Login.tsx
+++ client/screens/Login.tsx
@@ -1,4 +1,3 @@
-// @ts-ignore
 import React, { useState } from "react";
 import {
   Image,
@@ -10,6 +9,7 @@ import {
   useWindowDimensions,
   View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 // @ts-ignore
 import BgLogin from "../assets/images/bg-login.jpg";
 
@@ -36,7 +36,7 @@ const Login = ({ navigation }: LoginProps) => {
       return;
     } else {
       try {
-        fetch("http://192.168.9.22:8081/login", {
+        fetch("http://192.168.9.22:19000/login", {
           method: "POST",
           headers: {
             Accept: "application/json",
@@ -45,95 +45,66 @@ const Login = ({ navigation }: LoginProps) => {
           body: JSON.stringify(data),
           // @ts-ignore
         }).then((res) =>
-          // @ts-ignore
           res.json().then((data) => {
             if (data.error) {
               setErrors(data.message);
             } else {
-              // @ts-ignore
               alert("Login Successful");
               navigation.navigate("Home");
             }
           })
         );
       } catch (error) {
-        // @ts-ignore
         console.log(error);
       }
     }
   };
-  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+
   return (
-    // @ts-ignore
-    <View style={{ width: windowWidth, height: windowHeight }}>
-      {/* @ts-ignore */}
-      <View style={styles.container}>
-      {/* @ts-ignore */}
-        
-        <Image source={BgLogin} style={styles.patternbg} />
-      {/* @ts-ignore */}
-        
-        <View style={styles.container1}>
-      {/* @ts-ignore */}
-          
-          <ScrollView style={styles.containerLogin}>
-      {/* @ts-ignore */}
-            
-            <Text style={styles.welcomeBack}>Welcome Back</Text>
-      {/* @ts-ignore */}
-            
-            <Text style={styles.SigninText}>Enter your details below</Text>
-      {/* @ts-ignore */}
-
-            {errors ? <Text style={styles.error}>{errors}</Text> : null}
-      {/* @ts-ignore */}
-
-            <View>
-      {/* @ts-ignore */}
-
-              <Text style={styles.email}>Email</Text>
-      {/* @ts-ignore */}
-
-              <TextInput
-                onPressIn={() => setErrors(null)}
-                onChangeText={(text: any) => setData({ ...data, email: text })}
-                style={styles.input1}
-                placeholder="Enter your email"
-              />
-      {/* @ts-ignore */}
-
-              <Text style={styles.password}>Password</Text>
-      {/* @ts-ignore */}
-
-              <TextInput
-                onPressIn={() => setErrors(null)}
-                onChangeText={(text: any) =>
-                  setData({ ...data, password: text })
-                }
-                style={styles.input2}
-                secureTextEntry={true}
-                placeholder="Enter your password"
-              />
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Image source={BgLogin} style={styles.patternbg} />
+          <View style={styles.container1}>
+            <View style={styles.containerLogin}>
+              <Text style={styles.welcomeBack}>Welcome Back</Text>
+              <Text style={styles.SigninText}>Enter your details below</Text>
+              {errors ? <Text style={styles.error}>{errors}</Text> : null}
+              <View>
+                <Text style={styles.email}>Email</Text>
+                <TextInput
+                  onPressIn={() => setErrors(null)}
+                  onChangeText={(text: any) =>
+                    setData({ ...data, email: text })
+                  }
+                  style={styles.input1}
+                  placeholder="Enter your email"
+                />
+                <Text style={styles.password}>Password</Text>
+                <TextInput
+                  onPressIn={() => setErrors(null)}
+                  onChangeText={(text: any) =>
+                    setData({ ...data, password: text })
+                  }
+                  style={styles.input2}
+                  secureTextEntry={true}
+                  placeholder="Enter your password"
+                />
+              </View>
+              <TouchableOpacity onPressIn={sendToBackend}>
+                <Text style={styles.Login}>Login</Text>
+              </TouchableOpacity>
+              <Text
+                style={styles.Create}
+                onPress={() => navigation.navigate("Signup")}
+              >
+                Don't have an account?
+              </Text>
             </View>
-      {/* @ts-ignore */}
-
-            <TouchableOpacity onPressIn={sendToBackend}>
-      {/* @ts-ignore */}
-
-              <Text style={styles.Login}>Login</Text>
-            </TouchableOpacity>
-      {/* @ts-ignore */}
-
-            <Text
-              style={styles.Create}
-              onPress={() => navigation.navigate("Signup")}
-            >
-              Don't have an account?
-            </Text>
-          </ScrollView>
+          </View>
         </View>
-      </View>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -172,7 +143,7 @@ const styles = StyleSheet.create({
   },
   input1: {
     height: 40,
-    margin: 12,
+    margin: 13,
     borderWidth: 2,
     borderRadius: 10,
     padding: 13,
@@ -180,7 +151,7 @@ const styles = StyleSheet.create({
   },
   input2: {
     height: 40,
-    margin: 12,
+    margin: 13,
     borderWidth: 2,
     borderRadius: 10,
     padding: 13,
@@ -189,15 +160,14 @@ const styles = StyleSheet.create({
   containerLogin: {
     display: "flex",
     marginTop: "75%",
-    width: 415,
+    width: 410,
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     height: "100%",
     backgroundColor: "white",
-    
   },
   Login: {
-    fontFamily: "source-sans-pro",
+    fontFamily: 'quicksand-regular',
     color: "#ffffff",
     fontSize: 20,
     fontWeight: "bold",
@@ -205,12 +175,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#9848FF",
     padding: 10,
     borderRadius: 10,
-    margin: 10,
+    margin: 13,
   },
   welcomeBack: {
-    marginTop: "10%",
+    marginTop: "6%",
     fontSize: 40,
-    fontFamily: "source-sans-pro",
+    fontFamily: "quicksand-regular",
     color: "#000000",
     fontWeight: "bold",
     textAlign: "center",
@@ -238,20 +208,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     color: "#908F96",
   },
-  loginBtn: {
-    display: "flex",
-    marginTop: "10%",
-    marginLeft: "30%",
-    fontSize: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    fontWeight: "semibold",
-    color: "white",
-    backgroundColor: "#4b66e4",
-  },
   Create: {
     display: "flex",
-    marginTop: "5%",
+    marginTop: "2%",
     textAlign: "center",
     fontSize: 20,
     color: "#908F96",
